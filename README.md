56b73b41862d4b207b5d858b80c7e96a9973edc1
SamuelALSN <samuelgwfirst@gmail.com>
2020-05-21 10:24:12 +0000

    feat:adding analyseDiscussion function which analyse discussions based on details that we are looking for

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # whatsapp-chat-discussion-analyse
+This project is aim to analyse  a whatsapp chat discussions 
+
+So the app  will read your  whatsapp discussions  history file after downloading it .
+The  application will  prompt for the user's name on whom the analysis should run.
+
+The  application will display  the following details:
+
+Total number of messages sent by the entered user's username.
+
+Total number of times the user sent "lol" , "lmao" , emojis.
+
+Total number of profanities the user sent. The only profanities the app will check for is  "fuck", "merde", "putain", "ass".
+
+Total number of times the user recieved emojis.
+
+Total number of times the user recieved the angry 😡 emoji.
+
+Total number of times the user sent and recieved the words "amen", "akpe", "merci", "nagode", "imela", "thanks", "thank you", "alhamdulillah", "shukran".
+
+ ## Getting started 
+ clone the repository 
+ Go to the clone folder and run index.html 
--- index.js
+++ index.js
@@ -6,8 +6,8 @@ inputElement.addEventListener("change", () => {
     let reader = new FileReader()
     reader.onload = (e) => {
         const file = e.target.result;
-        removeLineBreak(file)
-        getUserMessages(file)
+       // removeLineBreak(file)
+        displayanalyseToUserInterface(file)
     }
 
     reader.readAsText(file)
@@ -18,7 +18,7 @@ inputElement.addEventListener("change", () => {
          * Returns array of lines.
          *
          * exemple:
-         *  turn
+         * fix
          * 04/08/2019 à 14:07 - Brook: Y a pas encore
          * Bcp de personnes
          * mais viens vite
@@ -71,18 +71,18 @@ inputElement.addEventListener("change", () => {
 
             }
         }
-        // console.log(users_and_messages)
+        //console.log(users_and_messages)
         return [users_and_messages, dates]
     }
 
     function getallUsers(file) {
+
         /**
          * Get all users in the discussions returned into an array
          *
          */
 
         let usersCleanedDiscussions = formatFile(file)[0]
-        //console.log(usersCleanedDiscussions)
         let userNames = []
         let tempUser
         for (let i = 0; i < usersCleanedDiscussions.length; i++) {
@@ -99,17 +99,94 @@ inputElement.addEventListener("change", () => {
     }
 
     function getUserMessages(file) {
+        /**
+         * Group for each user their messages into an object
+         * exemple:
+         *  user_with_messages = {
+         *      user_1 : ["message_1",......."message_N"]
+         *      .....                               ....
+         *      .....                                ....
+         *      user_N : ["message_1",......"message_N"]
+         *  }
+         *
+         */
         let users = getallUsers(file)
         let discussions = formatFile(file)[0]
-        console.log(users)
+        let messageByUsers = {}
+
         for (let i = 0; i < users.length; i++) {
-             let userRegex = new RegExp("^"+users[i])
+            let userRegex = new RegExp("^" + users[i])
             for (let j = 0; j < discussions.length; j++) {
-                if(userRegex.test(discussions[j])){
-                    console.log(users[i],discussions[j].split(": ")[1])
+                if (userRegex.test(discussions[j])) {
+                    // console.log(users[i], discussions[j].split(": ")[1])
+                    if (!messageByUsers[users[i]]) {
+                        messageByUsers[users[i]] = [discussions[j].split(": ")[1]]
+                    } else {
+                        messageByUsers[users[i]].push(discussions[j].split(": ")[1])
+                    }
                 }
             }
         }
 
+        // console.log(messageByUsers)
+        return messageByUsers
+    }
+
+    function analyseDiscussions(file) {
+        let user_with_messages = getUserMessages(file)
+
+        const wordsToLookFor = [
+            "lol", "lmao", "fuck", "merde", "putain", "ass",
+            "amen", "akpe", "merci", "nagode", "imela",
+            "thanks", "thank you", "alhamdulillah", "shukran"
+        ]
+
+        let allEmojiCheckingRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
+        let angryFaceEmojiRegex = /\uD83D\uDE21/g
+        let keyWordsPerUser = {}
+
+        for (let i = 0; i < wordsToLookFor.length; i++) {
+            let wordsToLookForRegex = new RegExp(wordsToLookFor[i], "gi")
+            // let wordsToLookForRegex = new RegExp("^"+wordsToLookFor[i]+"$", "gi")
+
+            for (let key in user_with_messages) {
+                let occurences = 0, totalEmojiPerUser = 0, totalAngryFaceEmojiPerUser = 0
+
+                for (let j = 0; j < user_with_messages[key].length; j++) {
+                    if (wordsToLookForRegex.test(user_with_messages[key][j])) {
+                        occurences += user_with_messages[key][j].match(wordsToLookForRegex).length
+                    }
+                    if (allEmojiCheckingRegex.test(user_with_messages[key][j])) {
+                        //console.log(user_with_messages[key][j].match(allEmojiCheckingRegex))
+                        totalEmojiPerUser += user_with_messages[key][j].match(allEmojiCheckingRegex).length
+                    }
+
+                    if (angryFaceEmojiRegex.test(user_with_messages[key][j])) {
+                       // console.log(user_with_messages[key][j].match(angryFaceEmojiRegex))
+                        totalAngryFaceEmojiPerUser += user_with_messages[key][j].match(angryFaceEmojiRegex).length
+                    }
+
+                }
+                if (!keyWordsPerUser[key]) {
+                    keyWordsPerUser[key] = {
+                        [wordsToLookFor[i]]: occurences
+                    }
+
+                } else {
+                    keyWordsPerUser[key][wordsToLookFor[i]] = occurences
+
+                }
+                keyWordsPerUser[key]["totalEmoji"] = totalEmojiPerUser
+                keyWordsPerUser[key]["angryFaceEmoji"] = totalAngryFaceEmojiPerUser
+            }
+        }
+       // console.log(keyWordsPerUser)
+
+        return keyWordsPerUser
+    }
+
+    function displayanalyseToUserInterface(file) {
+     let analyseResult = analyseDiscussions(file)
+        console.log(analyseResult)
     }
 })
